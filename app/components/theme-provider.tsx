ba89241af50c7d91cb5566686e561ed97a08eaf2
Vlademir Bastos <remotesismopro@gmail.com>
2025-07-16 00:41:29 -0300

    correcao theme

--- app/components/theme-provider.tsx
+++ app/components/theme-provider.tsx
@@ -2,7 +2,16 @@
 
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { type ThemeProviderProps } from "next-themes/dist/types"
+
+type ThemeProviderProps = {
+  children: React.ReactNode
+  attribute?: string
+  defaultTheme?: string | null
+  enableSystem?: boolean
+  disableTransitionOnChange?: boolean
+  storageKey?: string
+  themes?: string[]
+}
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
